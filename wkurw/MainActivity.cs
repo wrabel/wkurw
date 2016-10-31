6ed9ffb0df5c1f597d9ddac318f07c2304e2dd38
dawid <dawid@WRAB>
2016-10-31 19:26:05 +0100

    Add project files.

---
+++ wkurw/MainActivity.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using System.Collections.Generic;
+using Android.Support.V4.Widget;
+
+namespace wkurw
+{
+    [Activity(Label = "App1", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        DrawerLayout mDrawerLayout;
+        List<string> mLeftItems = new List<string>();
+        ArrayAdapter mLeftAdapter;
+        ListView mLeftDrawer;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.Main);
+            mDrawerLayout = FindViewById<DrawerLayout>(Resource.Id.Drawer);
+            mLeftDrawer = FindViewById<ListView>(Resource.Id.leftList);
+
+            mLeftItems.Add("Hello Word");
+            mLeftItems.Add("Kalkulator");
+
+            mLeftAdapter = new ArrayAdapter(this, Android.Resource.Layout.SimpleListItem1, mLeftItems);
+            mLeftDrawer.Adapter = mLeftAdapter;
+            
+                        Button button = FindViewById<Button>(Resource.Id.MyButton);
+
+                        button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+            
+        }
+    }
+}
+
